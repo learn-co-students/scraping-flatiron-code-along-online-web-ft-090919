ddaa5c6bb42209c7921068170ba924680222a0af
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-24 04:02:49 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,23 @@
 
 
+  class Course
+
+    attr_accessor :title, :schedule, :description, :class, :post
+
+    @@all = []
+
+    def initialize
+      @@all << self
+      self
+    end
+
+
+    def self.all
+      @@all
+    end
+
+    def self.reset_all
+      @@all.clear
+    end
+
+  end
