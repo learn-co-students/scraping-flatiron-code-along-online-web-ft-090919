9a2b164046536e902f56e3bc2b09869219335c63
Dominique Skaf <doskaf@yahoo.com>
2019-09-27 03:15:15 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,35 @@
-
+class Course
+  
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    @@doc = Nokogiri::HTML(html)
+  end
+  
+  def get_courses
+    @@courses = @@doc = doc.css("h2")
+  end
+  
+  def make_courses
+    @@courses.each do |course|
+      Course.new(course)
+    end
+  end
+  
+end
 
